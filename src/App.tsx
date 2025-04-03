eaf6485e093cc9b3fe57be21ed6aa171dd77ce78
Daliego <daliego82@gmail.com>
2025-04-02 23:02:13 -0300

    hotfix: fixing errors in hashed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { Theme } from "./layouts/theme";
-import { HashRouter, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom"; // Fixed import
+import { HashRouter } from "react-router-dom"; // Fixed import
 import { LoginPage } from "./screens/login";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { PublicationsPage } from "./screens/publications";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     Component: () => {
-//       return <PublicationsPage />;
-//     },
-//   },
-//   {
-//     path: "/login",
-//     Component: LoginPage,
-//   },
-// ]);
 
 const queryClient = new QueryClient();
 
@@ -25,19 +13,19 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Theme>
         <HashRouter basename="/">
+          <Toaster
+            toastOptions={{
+              style: {
+                borderRadius: "10px",
+                background: "#333",
+                color: "#fff",
+              },
+            }}
+            position="top-center"
+          />
           <Routes>
             <Route path="/" element={<PublicationsPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Toaster
-              toastOptions={{
-                style: {
-                  borderRadius: "10px",
-                  background: "#333",
-                  color: "#fff",
-                },
-              }}
-              position="top-center"
-            />
           </Routes>
         </HashRouter>
       </Theme>
