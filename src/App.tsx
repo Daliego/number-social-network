eaf9766c0f53ceb9ed6aaee918ad07cc98b01b3a
Daliego <daliego82@gmail.com>
2025-04-02 22:35:25 -0300

    horfix: added hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import { LoginPage } from "./screens/login";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { HashRouter } from "react-router-dom"; // Note 1
 import { PublicationsPage } from "./screens/publications";
 
 const router = createBrowserRouter([
@@ -24,17 +25,19 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Theme>
-        <RouterProvider router={router} />
-        <Toaster
-          toastOptions={{
-            style: {
-              borderRadius: "10px",
-              background: "#333",
-              color: "#fff",
-            },
-          }}
-          position="top-center"
-        />
+        <HashRouter>
+          <RouterProvider router={router} />
+          <Toaster
+            toastOptions={{
+              style: {
+                borderRadius: "10px",
+                background: "#333",
+                color: "#fff",
+              },
+            }}
+            position="top-center"
+          />
+        </HashRouter>
       </Theme>
     </QueryClientProvider>
   );
